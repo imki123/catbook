6d4dddfb29d7f0997f337330bbfd01657caea924
kiyoung <popping2606@gmail.com>
2020-09-22 13:59:38 +0900

    Updates

--- register-service-worker.js
+++
@@ -1,1 +0,0 @@
-"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/catbook/expo-service-worker.js",{scope:"/catbook/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
